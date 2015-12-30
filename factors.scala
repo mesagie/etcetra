0fe408e5c9245eca5a3138294aceb0e19360200e
Sagie Davidovich <mesagie@gmail.com>
2015-12-30 22:34:50 +0200

    Update factors.scala

--- factors.scala
+++ factors.scala
@@ -1,12 +1,11 @@
 object primeFactors{
   def factorize(x: Int): List[Int] = {
-  def foo(x: Int, a: Int): List[Int] = {
-    (a*a < x, x % a) match {
-        case (true, 0) => a :: foo(x/a, a)
-        case (true, _) => foo(x, a+1)
-        case (false, _) => List(x)
-      }
-    }
-    foo(x, 2)
-  }
+	@scala.annotation.tailrec
+	def foo(x: Int, a: Int = 2, list: List[Int] = Nil): List[Int] = a*a > x match {
+		case false if x % a == 0 => foo(x / a, a    , a :: list)
+		case false               => foo(x    , a + 1, list)
+		case true                => x :: list
+	}
+	foo(x)
+}
 }
